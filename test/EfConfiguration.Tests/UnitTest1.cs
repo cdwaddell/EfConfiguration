08fdbfa2ed759785ab0a2394033f5d7c84c291f2
CD Waddell <daniel.waddell@scholarrx.com>
2017-09-08 11:49:31 -0400

    Added unit test categories

--- test/EfConfiguration.Tests/UnitTest1.cs
+++ test/EfConfiguration.Tests/UnitTest1.cs
@@ -6,9 +6,17 @@ namespace EfConfiguration.Tests
     public class UnitTest1
     {
         [Fact]
+        [Trait("Category", "Unit")]
         public void Test1()
         {
+            Assert.NotNull(new object());
+        }
 
+        [Fact]
+        [Trait("Category", "Integration")]
+        public void Test2()
+        {
+            Assert.Null(new object());
         }
     }
 }
